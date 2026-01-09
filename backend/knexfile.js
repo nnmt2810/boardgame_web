54d1000559d2a28cb21eff30841031dbc3c7f271
23120102 <nnminhtuan2810@gmail.com>
2026-01-09 14:44:20 +0700

    feat: setup knex and database connection with supabase

---
+++ backend/knexfile.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    }
+  }
+};
